f8cd238e48869192c0c9e57bbc50bc6a054f29c9
Adam Wells <adam@interactiveknowledge.com>
2019-08-22 17:58:11 -0400

    Fixed bug with Herousel sliding

--- src/scripts/components/Herousel.js
+++ src/scripts/components/Herousel.js
@@ -30,34 +30,65 @@ export default class Herousel {
     this.parent.style.height = `${totalHeight}px`;
   }
 
+  /**
+   * Method for sliding to the next slide in the Herousel
+   */
   slideNext() {
     this.currentIndex++;
+
+    // We need a separate index to select our animation slide
+    let animationIndex = this.currentIndex - 1;
+    if (animationIndex > this.slides.length - 1) {
+      animationIndex = 0;
+    }
 
     if (this.currentIndex > this.slides.length - 1) {
       this.currentIndex = 0;
     }
 
-    // TODO: Fix bug on last slide.
-    this.slides[this.currentIndex - 1].classList.add('out');
+    // Out the out class for the animation out
+    this.slides[animationIndex].classList.add('out');
+
+    // After 500ms, toggle the active class and remove the out class
     setTimeout(() => {
       Array.from(this.slides).forEach((slide, i) => {
         slide.classList.toggle('active', i === this.currentIndex);
       });
-      this.slides[this.currentIndex - 1].classList.remove('out');
+      this.slides[animationIndex].classList.remove('out');
     }, 500);
+
+    // Reset the parentHeight
     this.setParentHeight();
   }
 
+  /**
+   * Method for sliding to the prev slide in the Herousel
+   */
   slidePrev() {
     this.currentIndex--;
+
+    // We need a separate index to select our animation slide
+    let animationIndex = this.currentIndex + 1;
+    if (animationIndex < 0) {
+      animationIndex = this.slides.length - 1;
+    }
 
     if (this.currentIndex < 0) {
       this.currentIndex = this.slides.length - 1;
     }
 
-    Array.from(this.slides).forEach((slide, i) => {
-      slide.classList.toggle('active', i === this.currentIndex);
-    });
+    // Out the out class for the animation out
+    this.slides[animationIndex].classList.add('out');
+
+    // After 500ms, toggle the active class and remove the out class
+    setTimeout(() => {
+      Array.from(this.slides).forEach((slide, i) => {
+        slide.classList.toggle('active', i === this.currentIndex);
+      });
+      this.slides[animationIndex].classList.remove('out');
+    }, 500);
+
+    // Reset the parentHeight
     this.setParentHeight();
   }
 
