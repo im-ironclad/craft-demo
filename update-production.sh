bd266cf5a7bfef6dacd75db9b12a0c7f49216a0c
Adam Wells <adam@interactiveknowledge.com>
2019-09-04 12:15:52 -0400

    Adjusting update-production script and added env.sh example file

--- update-production.sh
+++ update-production.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-##
+source .env.sh
+
+# Constants
+BACKUP="db.sql"
+ASSET_PATH="public_html/uploads"
+
 # Variables
-##
-# Local
-LOCAL_DB_NAME=""
-LOCAL_DB_USER=""
-LOCAL_DB_PASS=""
-LOCAL_HOME_PATH=""
-# Remote
-REMOTE_DB_NAME=""
-REMOTE_DB_USER=""
-REMOTE_DB_PASS=""
-REMOTE_HOME_PATH=""
+REMOTE_DB_NAME="$PRODUCTION_DB_NAME"
+REMOTE_DB_USER="$PRODUCTION_DB_USER"
+REMOTE_DB_PASS="$PRODUCTION_DB_PASS"
+REMOTE_HOME_PATH="$PRODUCTION_HOME_PATH"
 
-# SSH
-SSH_HOST="ik-craft-demo.com@ssh.stackcp.com"
+# Steps: Ignore owner, group, and permissions on push rsync
+# 1) Dump DB
+# 2) Sync DB
+# 3) Import DB
+# 4) Sync assets
+# 5) Remove DB backup
+mysqldump -u"$LOCAL_DB_USER" -p"$LOCAL_DB_PASS" "$LOCAL_DB_NAME" > "$LOCAL_HOME_PATH/$BACKUP"
 
-##
-# Actual scripts to run
-##
-ssh "$SSH_HOST" "git fetch && git pull origin master"
+# OLD: ssh "$SSH_HOST" "git fetch && git pull origin master"
